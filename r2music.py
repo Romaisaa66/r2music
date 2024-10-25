75472d709f89066c0c681d381fc19477b3ac1b6e
Romaisaa66 <remoyah66@hotmail.com>
2024-10-25 09:13:23 +0400

    Fix app definition

--- r2music.py
+++ r2music.py
@@ -178,3 +178,13 @@ if __name__ == '__main__':
 
 def app():
     return None
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # تأكد من وجود ملف index.html
+
+if __name__ == '__main__':
+    app.run(port=7777)
